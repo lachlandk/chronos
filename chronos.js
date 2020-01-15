3dc32fb538477ff8863244dda3f12534bfa4fb84
Lachlan <lachlandufort-kennett@outlook.com>
2020-01-15 18:34:27 +0000

    Added stopwatch

--- chronos.js
+++ chronos.js
@@ -1,7 +1,25 @@
-(function (root, factory){
+(function (root, factory) {
 	root.Chronos = factory();
-}(window, function (){
+}(window, function () {
 	return {
-
+		stopWatch: function () {
+			return new function () {
+				this._startTime = new Date().getTime();
+				this.time = function (units = null) {
+					if (units === null) {
+						return new Date().getTime() - this._startTime;
+					} else if (typeof units === "string") {
+						switch (units) {
+							case "seconds":
+								return (new Date().getTime() - this._startTime) / 1000;
+							case "minutes":
+								return (new Date().getTime() - this._startTime) / (1000 * 60);
+							case "hours":
+								return (new Date().getTime() - this._startTime) / (1000 * 3600);
+						}
+					}
+				};
+			}();
+		}
 	}
 }));
